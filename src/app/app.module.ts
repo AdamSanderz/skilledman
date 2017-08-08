e7f7621dba3c5c08f20ac59edd17ffba5bf66980
Payam Omrani <payam.omrani@gmail.com>
2017-08-08 18:40:15 +0430

    done working with the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpModule} from "@angular/http";
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +28,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,82 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, LoadingController, AlertController, Loading} from 'ionic-angular';
+import {Http, Headers, RequestOptions} from "@angular/http";
+import { Storage } from '@ionic/storage';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  loading: Loading;
+  registerCredentials = {mobile: '',email: '', password: '', response: ''};
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController, public storage: Storage, public alertCtrl: AlertController) {
+    this.checkuser();
+  }
+
+
+  login() {
+    let headers = new Headers();
+    // headers.append("Accept", 'application/json');
+    // headers.append('Content-Type', 'application/json');
+    headers.append('API_KEY', 'YXV0b2FwcGFwaWtleTEyMzQ1Ng==');
+    let options = new RequestOptions({headers: headers});
+
+    // let postParams = {
+    //   mobile: '09123227346',
+    //   password: '123456'
+    //
+    // };
+    let postParams = {
+      mobile: this.registerCredentials.mobile,
+      password: this.registerCredentials.password
+
+    };
+
+    this.http.post("http://127.0.0.1:8000/api/users/newlogin", postParams, options)
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);// Error getting the data
+      });
+  }
+
+
+  showLoading() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    loading.present();
+  }
+
+  checkuser() {
+    this.storage.get('name').then((val) => {
+      console.log('Your mobile is', val);
+    });
+
+    this.storage.get('mobile').then((mobile) => {
+      console.log('Your mobile is', mobile);
+    });
 
   }
 
-}
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+
+
+
+
+  }
